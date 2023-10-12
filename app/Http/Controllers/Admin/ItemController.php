d90933c9788a7f63cd19133a96112e730332a20b
RaihanSnh <nathahamzahraihansatya@gmail.com>
2023-10-12 12:19:54 +0700

    Add Middleware, Traits, Controller

---
+++ app/Http/Controllers/Admin/ItemController.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Item;
+use App\Services\Admin\ItemService;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use function back;
+
+class ItemController{
+
+	public function create(Request $request) {
+		$request->validate([
+			'name' => 'required',
+			'description' => 'required',
+			'series' => 'required',
+			'total' => 'required',
+			'image' => 'required|mimes:png,jpg',
+		]);
+
+		ItemService::getInstance()->create(
+			$request->post('name'),
+			(int) $request->get('warehouse_id'),
+			$request->post('description'),
+			$request->post('series'),
+			$request->post('total'),
+			$request->post('image'),
+			(int) $request->get('detail_item_id'),
+		);
+		$request->session()->flash('message', 'Item added');
+		return back();
+	}
+
+	public function update(Item $item, Request $request) {
+		$request->validate([
+			'name' => 'required',
+			'description' => 'required',
+			'series' => 'required',
+			'total' => 'required',
+			'image' => 'required|mimes:png,jpg'
+		]);
+
+		ItemService::getInstance()->update(
+			$item,
+			$request->post('name'),
+			(int) $request->get('warehouse_id'),
+			$request->post('description'),
+			$request->post('series'),
+			$request->post('total'),
+			$request->post('image'),
+			(int) $request->get('detail_item_id'),
+		);
+		$request->session()->flash('message', 'Item added');
+		return back();
+	}
+
+	public function delete(Item $item, Request $request) {
+		$item->delete();
+
+		$request->session()->flash('message', 'Item deleted');
+		return back();
+	}
+}
