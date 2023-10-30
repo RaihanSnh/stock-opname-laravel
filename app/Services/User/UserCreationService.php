ce141fb33d4b31c08d507d809c7bdafd05bb1594
syahrulapra <syahrulap27@gmail.com>
2023-10-30 20:15:53 +0700

    conflict

--- app/Services/User/UserCreationService.php
+++ app/Services/User/UserCreationService.php
@@ -17,84 +17,31 @@ class UserCreationService
 
 	use SingletonTrait;
 
-	public function createRequester(string $username, string $email, string $password, ?UploadedFile $image, string $date_of_birth, string $ein, string $gender) : void
-	{
+	public function createUser(string $username, string $email, string $password, ?UploadedFile $image, string $date_of_birth, string $ein, string $gender, string $role): void
+    {
+        $user = $this->create($username, $email, $password, $date_of_birth, $ein, $gender, $role);
 
-		$date_of_birth = \DateTime::createFromFormat('Y-m-d', $date_of_birth);
-		if ($date_of_birth !== false) {
-			$date_of_birth = $date_of_birth->format('Y-m-d');
-		}
+        $this->processImage($user, $image, 'images/user');
 
-		$user = $this->create($username, $password, $email, $image, $date_of_birth, $ein, $gender, User::ROLE_REQUESTER);
+        $user->save();
+    }
 
-		$requester = new Requester();
-		$requester->user_id = $user->id;
-
-		if($image !== null){
-			$image->move(public_path('images/requester'), $fileName = Str::random(16) . '.' . $image->extension());
-			$requester->image = $fileName;
-		}
-
-		$requester->save();
-	}
-
-	public function createWarehouseStaff(string $username, string $email, string $password, ?UploadedFile $image, string $date_of_birth, string $ein, string $gender) : void
-	{
-
-		$date_of_birth = \DateTime::createFromFormat('Y-m-d', $date_of_birth);
-		if ($date_of_birth !== false) {
-			$date_of_birth = $date_of_birth->format('Y-m-d');
-		}
-
-		$user = $this->create($username, $password, $email, $image, $date_of_birth, $ein, $gender, User::ROLE_WAREHOUSE_STAFF);
-
-		$warehouse_staff = new WarehouseStaff();
-		$warehouse_staff->user_id = $user->id;
-
-		if($image !== null){
-			$image->move(public_path('images/warehouse_staff'), $fileName = Str::random(16) . '.' . $image->extension());
-			$warehouse_staff->image = $fileName;
-		}
-
-		$warehouse_staff->save();
-	}
- 
-	public function createAdmin(string $username, string $email, string $password, ?string $imagePath = null, string $date_of_birth, string $ein, string $gender) : void
-	{
-
-		$date_of_birth = \DateTime::createFromFormat('Y-m-d', $date_of_birth);
-        if ($date_of_birth !== false) {
-            $date_of_birth = $date_of_birth->format('Y-m-d');
-        }
-
-		$user = $this->create($username, $password, $email, $imagePath, $date_of_birth, $ein, $gender, User::ROLE_ADMIN);
-
-		if ($imagePath !== null && file_exists($imagePath)) {
-			$image = new UploadedFile($imagePath, basename($imagePath));
-			$fileName = Str::random(16) . '.' . $image->extension();
-			$image->move(public_path('images/admin'), $fileName);
-			$user->image = $fileName;
-		} else {
-			$user->image = 'default.jpg'; 
-		}
-		$user->save();
-	}
-
-	private function create(string $username, string $password, string $email, string $role, ?string $image = null, string $date_of_birth, string $ein, string $gender) : User
-	{
-		$user = new User();
-		$user->name = $username;
-		$user->email = $email;
-		$user->setPassword($password);
-		$user->role = $role;
-		$user->image = 'default.jpg';
-		$user->date_of_birth = \DateTime::createFromFormat('Y-m-d', $date_of_birth);
-		$user->ein = $ein;
-		$user->gender = $gender;
+	private function create(string $username, string $email, string $password, string $date_of_birth, string $ein, string $gender, string $role): User
+    {
+        $user = new User();
+        $user->ein = $ein;
+        $user->name = $username;
+        $user->email = $email;
+        $user->password = Hash::make($password);
+        $user->gender = $gender;
+        $user->date_of_birth = $date_of_birth;
+        $user->role = $role;
+        $user->image = 'default.jpg';
 
 		$user->save();
+	
 		return $user;
-	}
+    }
 
     private function processImage(User $user, ?UploadedFile $image, string $destination): void
     {
